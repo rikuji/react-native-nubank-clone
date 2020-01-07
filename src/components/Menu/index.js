461dd40c55527272983eee72590d04b15c2d3cf5
Lucas Bezerra <lucasbezerra1701@gmail.com>
2020-01-06 22:15:27 -0300

    First Commit

---
+++ src/components/Menu/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import QRCode from 'react-native-qrcode-svg';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Code, Nav, NavItem, NavText, SignOutButton, SignOutButtonText } from './styles.js';
+
+export default function Menu ({ translateY }) {
+  return (
+    <Container
+      style={{
+        opacity: translateY.interpolate({
+          inputRange: [0, 150],
+          outputRange: [0, 1]
+        })
+      }}
+    >
+      <Code>
+        <QRCode
+          value="https://rocketseat.com.br"
+          size={80}
+          fgColor="#fff"
+          bgColor="#8B10AE"
+        />
+      </Code>
+
+      <Nav>
+        <NavItem>
+          <Icon name="help-outline" size={20} color="#FFF" />
+          <NavText>Me ajuda</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="person-outline" size={20} color="#FFF" />
+          <NavText>Perfil</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="credit-card" size={20} color="#FFF" />
+          <NavText>Configurar cartão</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="smartphone" size={20} color="#FFF" />
+          <NavText>Confirgurações do app</NavText>
+        </NavItem>
+      </Nav>
+
+      <SignOutButton onPress={() => { }}>
+        <SignOutButtonText>SAIR DO APP</SignOutButtonText>
+      </SignOutButton>
+    </Container>
+  );
+}
